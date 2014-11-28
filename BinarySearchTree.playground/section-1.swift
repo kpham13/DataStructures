7a48d5625652bad7ee4aa2eb394944175afc7e0e
Kevin Pham <kevin.v.pham@hotmail.com>
2014-11-28 09:03:15 -0800

    binary search tree

---
+++ BinarySearchTree.playground/section-1.swift
@@ -0,0 +1,15 @@
+//////////////////////////////////////////
+// Data Structures - Binary Search Tree //
+//////////////////////////////////////////
+
+// Hierarchical structure with root node, subtrees, and children/leaf nodes.
+// Ordered or sorted binary tree (specialized type of tree, where each node has at most two children.
+// The left subtree of a node contains only nodes with keys less than the node's key.
+// The right subtree of a node contains only nodes with keys greater than the node's key.
+// The left and right subtree each must be a binary search tree.
+// Each node can have up to two successor nodes.
+// There must be no duplicate nodes.
+// A unique path exists from the root to every other node.
+
+import Foundation
+
